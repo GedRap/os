8f99d1cf1e882150706721dda67bc5cbd3dacdb5
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2014-03-04 05:32:44 +0000

    Allocating stack memory for tasks

--- memory.c
+++ memory.c
@@ -2,39 +2,11 @@
  * memory.c
  *
  * Memory management related code
- */ 
+ */
 
 #include "memory.h"
 
-//SP manipulation
-volatile inline unsigned int os_get_sp() {
-	asm volatile(
-	"push r0 \n"
-	"in r0, __SP_L__ \n"
-	"push r26 \n"
-	"push r27 \n"
-	"lds r26, os_global_sp_ptr \n"
-	"lds r27, os_global_sp_ptr+1 \n"
-	"st X+, r0 \n"
-	"in r0, __SP_H__ \n"
-	"st X+, r0 \n"
-	"pop r27 \n"
-	"pop r26 \n"
-	"pop r0 \n"
-	);
-	
-	return os_global_sp_addr;
-}
+char os_task_stack0[OS_TASK_STACK_SIZE];
+char os_task_stack1[OS_TASK_STACK_SIZE];
 
-volatile inline char os_set_sp() {
-	asm volatile(
-	"lds r26, os_global_sp_ptr \n"
-	"lds r27, os_global_sp_ptr + 1 \n"
-	"ld r28, X+ \n"
-	"out __SP_L__, r28 \n"
-	"ld r29, X+ \n"
-	"out __SP_H__, r29 \n"
-	);
-	
-	return 1;
-}
+os_stack os_task_stacks[] = {&os_task_stack0, &os_task_stack1};
--- memory.h
+++ memory.h
@@ -8,6 +8,8 @@
 #ifndef MEMORY_H_
 #define MEMORY_H_
 
+#define OS_TASK_STACK_SIZE 128
+#define OS_TASK_STACKS 2
 
 //SP address
 //Used by get_sp and set_sp methods. Has to be global so that I could use a label
@@ -15,10 +17,39 @@
 extern unsigned int os_global_sp_addr;
 extern unsigned int *os_global_sp_ptr;
 
-//get SP and save it's address to os_global_sp_addr
-extern volatile inline unsigned int os_get_sp();
-//overwrite existing SP address with a value found in os_global_sp_ptr
-extern volatile inline char os_set_sp();
+extern unsigned int os_task_sp_addr;
 
+//get SP and save it's address to os_global_sp_addr
+#define os_get_sp() \
+	asm volatile( \
+	"push r0 \n" \
+	"in r0, __SP_L__ \n" \
+	"push r26 \n" \
+	"push r27 \n" \
+	"lds r26, os_global_sp_ptr \n" \
+	"lds r27, os_global_sp_ptr+1 \n" \
+	"st X+, r0 \n" \
+	"in r0, __SP_H__ \n" \
+	"st X+, r0 \n" \
+	"pop r27 \n" \
+	"pop r26 \n" \
+	"pop r0 \n" \
+	);
+//overwrite existing SP address with a value found in os_global_sp_ptr
+#define os_set_sp()  \
+	asm volatile( \
+	"lds r26, os_global_sp_ptr \n" \
+	"lds r27, os_global_sp_ptr + 1 \n" \
+	"ld r28, X+ \n" \
+	"out __SP_L__, r28 \n" \
+	"ld r29, X+ \n" \
+	"out __SP_H__, r29 \n" \
+	);
+
+typedef char* os_stack;
+
+extern os_stack os_task_stacks[OS_TASK_STACKS];
+extern char os_task_stack0[OS_TASK_STACK_SIZE];
+extern char os_task_stack1[OS_TASK_STACK_SIZE];
 
 #endif /* MEMORY_H_ */
